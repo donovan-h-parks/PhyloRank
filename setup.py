392d97f5ce4c95cb147cf1286d37229265b30577
dparks1134 <donovan.parks@gmail.com>
2020-03-12 02:28:58 +1000

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -7,17 +7,30 @@ def version():
     versionFile = open(os.path.join(setupDir, 'phylorank', 'VERSION'))
     return versionFile.readline().strip()
 
+data_files = []
+js_files = []
+js_dir = './phylorank/mpld3/mpld3/js'
+for f in os.listdir(js_dir):
+    js_files.append(os.path.join(js_dir, f))
+data_files.append((js_dir, js_files))
+
 setup(
     name='phylorank',
     version=version(),
     author='Donovan Parks',
     author_email='donovan.parks@gmail.com',
-    packages=['phylorank', 'phylorank.plot'],
+    packages=['phylorank', 
+                'phylorank.plot', 
+                'phylorank.mpld3.mpld3', 
+                'phylorank.mpld3.mpld3',
+                'phylorank.mpld3.mpld3.mplexporter',
+                'phylorank.mpld3.mpld3.mplexporter.renderers'],
     scripts=['bin/phylorank'],
     package_data={'phylorank' : ['VERSION']},
+    data_files=data_files,
     url='http://pypi.python.org/pypi/phylorank/',
     license='GPL3',
     description='Assigns taxonomic ranks based on evolutionary divergence.',
     install_requires=[
-        "biolib >= 0.0.46"],
+        "biolib >= 0.1.0"],
 )
